a17174fb0d686a20c3b5f729f09cef9efcdc0a41
gujarminar-tech <gujarminar@gmail.com>
2026-02-19 23:17:01 +0530

    for loop

--- prgramming in python/11_loops.py
+++ prgramming in python/11_loops.py
@@ -1,25 +1,25 @@
-# #enumerate
-# list=[10,20,30,40,50]
-# for idx,val in enumerate(list):
-#     print(idx,val)
+#enumerate
+list=[10,20,30,40,50]
+for idx,val in enumerate(list):
+    print(idx,val)
 
-# #nested list
-# list1=[["Virat","Rohit"],["Sachin","Dhoni"],["Hardik","Surya"]]
-# for a in list1:
-#     for b in a:
-#         print(b)
+#nested list
+list1=[["Virat","Rohit"],["Sachin","Dhoni"],["Hardik","Surya"]]
+for a in list1:
+    for b in a:
+        print(b)
 
-# #zip
-# parties=["Congress","BJP","X"]
-# Seats=[100,200,50]
-# for a,b in zip(parties,Seats):
-#     print(a,b)
+#zip
+parties=["Congress","BJP","X"]
+Seats=[100,200,50]
+for a,b in zip(parties,Seats):
+    print(a,b)
 
-# parties=["Congress","BJP","X","Y"]
-# Seats=[100,200,50]
-# for a,b in zip(parties,Seats):
-#     print(a,b)
-# # no. of loops=min{len(parties),len(Seats)}
+parties=["Congress","BJP","X","Y"]
+Seats=[100,200,50]
+for a,b in zip(parties,Seats):
+    print(a,b)
+# no. of loops=min{len(parties),len(Seats)}
 
 List=[10,20,30,40]
 tuple=(11,23,45)
