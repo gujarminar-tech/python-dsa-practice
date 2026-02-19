0e0929f8bf8eae2c5e809323b13c56d62506ba1a
gujarminar-tech <gujarminar@gmail.com>
2026-02-19 23:14:11 +0530

    else in loops

--- prgramming in python/12_whileloop.py
+++ prgramming in python/12_whileloop.py
@@ -1,33 +1,33 @@
-# fruits=["Apple","Banana","Orange"]
-# i=0
-# n=len(fruits)
-# while i<n:
-#     print(fruits[i])
-#     i+=1
+fruits=["Apple","Banana","Orange"]
+i=0
+n=len(fruits)
+while i<n:
+    print(fruits[i])
+    i+=1
 
-# #break
-# lst=[30,20,40,10,60,70,80]
-# total=0
-# for item in lst:
-#     total+=item
-#     if total>100:
-#         break
-#     print(total)
+#break
+lst=[30,20,40,10,60,70,80]
+total=0
+for item in lst:
+    total+=item
+    if total>100:
+        break
+    print(total)
 
 #continue
-# a=[5,-3,2,4,-5,0,-6,9]
-# for item in a:
-#     if item<0:
-#         continue
-#     print(item)
+a=[5,-3,2,4,-5,0,-6,9]
+for item in a:
+    if item<0:
+        continue
+    print(item)
 
-# #pass 
-# l1=[10,20,30,40,50]
-# for a in l1:
-#     if a>30:
-#         pass #empty statement
-#     print(a)
-
+#pass 
+l1=[10,20,30,40,50]
+for a in l1:
+    if a>30:
+        pass #empty statement
+    print(a)
+    
 # else in loops 
 l2=[10,20,30,40]
 for item in l2:
