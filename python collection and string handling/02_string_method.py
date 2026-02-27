b19036d28b2f63a63d28b649d1fe1d5a122e370d
gujarminar-tech <gujarminar@gmail.com>
2026-02-28 00:01:52 +0530

    replace method

--- python collection and string handling/02_string_method.py
+++ python collection and string handling/02_string_method.py
@@ -34,3 +34,12 @@ print(str5.rsplit(",",maxsplit=3))
 l="12345"
 print(" ".join(l))
 print("_".join("GATE"))
+
+#replace method
+str1="I like python"
+print(str1.replace("I","They"))
+str2="Hello hello"
+print(str2.replace("Hello","Hi"))
+str3="hello hello hello"
+print(str3.replace("hello","hi"))
+print(str3.replace("hello","hi",2))# bydefault from left to right
---
+++ python collection and string handling/03_string_method2.0.py
@@ -0,0 +1,8 @@
+#replace method
+str1="I like python"
+print(str1.replace("I","They"))
+str2="Hello hello"
+print(str2.replace("Hello","Hi"))
+str3="hello hello hello"
+print(str3.replace("hello","hi"))
+print(str3.replace("hello","hi",2))# bydefault from left to right
