a1ba415ea5636ea07115a13d2d6382918783d625
gujarminar-tech <gujarminar@gmail.com>
2026-03-01 18:28:30 +0530

    indexing in list

--- python collection and string handling/04_list.py
+++ python collection and string handling/04_list.py
@@ -5,3 +5,10 @@ lst1=list()
 lst2=list("Hello")
 print(lst2)#["H","e","l","l","o"]
 
+
+#indexing
+lst3=[[1,2,3,[4,5]],[6,[7,8]],9,10,11]
+print(len(lst3))#5
+print(lst3[0][1])#2
+print(lst3[0][3][1])#5
+print(lst3[-4][-1][-2])#7
