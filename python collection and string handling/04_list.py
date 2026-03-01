b16f551a4fe52545f1001302f896a8ec2bb18fd6
gujarminar-tech <gujarminar@gmail.com>
2026-03-01 23:12:24 +0530

    slicing in list

--- python collection and string handling/04_list.py
+++ python collection and string handling/04_list.py
@@ -12,3 +12,9 @@ print(len(lst3))#5
 print(lst3[0][1])#2
 print(lst3[0][3][1])#5
 print(lst3[-4][-1][-2])#7
+
+#slicing
+lst1=["a","b","c","d","e","f","g","h","i","j"]
+print(lst1[0:5])#['a', 'b', 'c', 'd', 'e']
+print(lst1[2:9:2])#['c', 'e', 'g', 'i']
+print(lst1[-4:9])#['g', 'h', 'i']
