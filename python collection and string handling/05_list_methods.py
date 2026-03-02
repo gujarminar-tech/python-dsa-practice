e6bf51eea62520f6f290ef1dabea16d05089c402
gujarminar-tech <gujarminar@gmail.com>
2026-03-02 12:40:04 +0530

    list as input from user

--- python collection and string handling/05_list_methods.py
+++ python collection and string handling/05_list_methods.py
@@ -15,3 +15,15 @@ lst2.extend("Hey")
 print(lst2)#[1, 2, 3, 'H', 'e', 'y']
 lst2.extend([4,5,6])
 print(lst2)#[1, 2, 3, 'H', 'e', 'y', 4, 5, 6]
+
+
+#list as input from user
+lst1=input("Enter values : ").split()
+print(lst1) 
+#but the only problem is each element is in string form
+
+lst2=[]
+count=int(input("Enter number of values: "))
+for i in range(count):
+    lst2.append(int(input("enter value: ")))
+print(lst2)
