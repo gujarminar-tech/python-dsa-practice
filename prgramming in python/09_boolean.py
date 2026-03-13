21d77091c8ad5dbd7d8510d83771b34b5fdec8de
gujarminar-tech <gujarminar@gmail.com>
2026-03-13 20:09:04 +0530

    bool condition for True

--- prgramming in python/09_boolean.py
+++ prgramming in python/09_boolean.py
@@ -4,3 +4,10 @@ print(bool("")) #empty string
 print(bool([])) #empty list
 print(bool(())) #empty set
 print(bool({})) #empty dictionary
+
+
+print(bool(" "))
+print(bool(0.1))
+print(bool(1))
+print(bool([0]))
+print(bool((1)))
