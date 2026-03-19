2e05ee8dae7fefde68ed4d9a5df9b099b4087d57
gujarminar-tech <gujarminar@gmail.com>
2026-03-20 02:16:08 +0530

    initialize set

---
+++ python collection and string handling/07_set.py
@@ -0,0 +1,11 @@
+'''mutable
+unordered
+duplicates are not allowed
+imp* In setr only immutable objects are allowed 
+mutrable objects are not allowed like (List,dist,set)
+'''
+
+s1={1,0,True,False,0,0.0}
+#initialize empty set
+set1=set() 
+print(type(set1))
