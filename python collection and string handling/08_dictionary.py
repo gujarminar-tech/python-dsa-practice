01c44d92d196df981d894bfe1d1e5ecb6c656e05
gujarminar-tech <gujarminar@gmail.com>
2026-03-25 01:07:26 +0530

    initalization in dict

---
+++ python collection and string handling/08_dictionary.py
@@ -0,0 +1,9 @@
+#initialization of dictionary
+dict={}
+dict1=dict()
+dict2={"a":1,"b":2,"c":3}
+dict3={"a":9,"b":[2,3,4],(2,3):{2,3,4}}
+#key always immutable 
+#list,set,dict are not allowed at key
+#duplicate key not allowed
+#value can be anything even repetition is allowed
