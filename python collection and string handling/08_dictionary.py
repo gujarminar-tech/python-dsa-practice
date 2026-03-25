4919656247ed38f6037d1576eb39ce1aed80596a
gujarminar-tech <gujarminar@gmail.com>
2026-03-25 16:49:26 +0530

    creating dict methods

--- python collection and string handling/08_dictionary.py
+++ python collection and string handling/08_dictionary.py
@@ -1,5 +1,5 @@
 #initialization of dictionary
-dict={}
+dict1={}
 dict2={"a":1,"b":2,"c":3}
 dict3={"a":9,"b":[2,3,4],(2,3):{2,3,4}}
 #key always immutable 
@@ -17,3 +17,17 @@ print(d1[(3,4)])
 #key should not be duplicate
 d1={1:"A",0:"x",1.0:"ab",True:"Y",False:"G","1":"r","0":"F"}
 print(d1) #{1: 'Y', 0: 'G', '1': 'r', '0': 'F'
+
+#creating dict methods 
+d1=dict(a=1,b=2,c=3,d="a")
+print(d1) #{'a': 1, 'b': 2, 'c': 3, 'd': 'a'}
+
+d1={"a":1,"b":2,"c":3}
+d2=d1
+d3=dict(d1)
+d4=d1.copy()
+d1["a"]=4
+print(d1) #{'a': 4, 'b': 2, 'c': 3}
+print(d2) #{'a': 4, 'b': 2, 'c': 3} change with d1
+print(d3) #{'a': 1, 'b': 2, 'c': 3} create copy
+print(d4) #{'a': 1, 'b': 2, 'c': 3}
