588a0edaad4e92e872cfbc12b1d58c1f239ae772
gujarminar-tech <gujarminar@gmail.com>
2026-03-27 00:23:46 +0530

    accessing elements in tuple

--- python collection and string handling/09_tuple.py
+++ python collection and string handling/09_tuple.py
@@ -25,3 +25,19 @@ a,*b,c=(1,3,4,5,6)
 print(b) #[3,4,5]
 print(a) #1
 print(c) #6
+
+#accessing elements in tuple
+t1=(1,2,3,4)
+print(t1[1])
+print(t1[-2])
+print(t1[1:3]) #(2, 3)
+
+list=[10,20,30,40,50]
+t2=(1,2,list)
+print(t2)
+list[0]=30
+print(t2) #(1, 2, [30, 20, 30, 40, 50])
+#tuple can store mutable values
+t2[2][1]=60
+print(t2) #(1, 2, [30, 60, 30, 40, 50])
+# t2[2]=[30,80] #error\
