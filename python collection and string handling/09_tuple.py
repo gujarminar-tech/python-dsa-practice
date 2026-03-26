f29d3fe58ee9bcecce55342f253b3c3da9cd87cf
gujarminar-tech <gujarminar@gmail.com>
2026-03-26 15:55:27 +0530

    tuple unpacking

--- python collection and string handling/09_tuple.py
+++ python collection and string handling/09_tuple.py
@@ -15,3 +15,13 @@ print(type(t4)) #<class 'tuple'>
 t5=1,2,3,4
 print(type(t5)) #<class 'tuple'>
 
+
+#tuple Unpacking
+a,b,c=(1,2,3)
+print(a)
+print(b)
+print(c)
+a,*b,c=(1,3,4,5,6)
+print(b) #[3,4,5]
+print(a) #1
+print(c) #6
