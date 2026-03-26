4bc112b33d932ccfed096e0aa20425f6c06c3fc4
gujarminar-tech <gujarminar@gmail.com>
2026-03-26 14:47:41 +0530

    tuple initialization

---
+++ python collection and string handling/09_tuple.py
@@ -0,0 +1,17 @@
+#immutable
+#ordered 
+#indexing
+#slicing
+
+#tuple Initialization
+t1=()
+t1=tuple()
+t2=(1)
+print(type(t2)) #<class 'int'>
+t3=1,
+print(type(t3)) #<class 'tuple'>
+t4=(1,)
+print(type(t4)) #<class 'tuple'>
+t5=1,2,3,4
+print(type(t5)) #<class 'tuple'>
+
