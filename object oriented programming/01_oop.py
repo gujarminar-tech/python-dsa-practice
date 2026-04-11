57ad8e4f09802e7d7bea4454eb051de7bcce1e02
gujarminar-tech <gujarminar@gmail.com>
2026-04-11 18:29:45 +0530

    create constructor

---
+++ object oriented programming/01_oop.py
@@ -0,0 +1,14 @@
+class Car:
+    def __init__(self,color,model,brand):
+        self.brand=brand
+        self.color=color
+        self.model=model
+
+car1=Car("Red","Nexon","Tata")
+
+class student:
+    def __init__(self,name,rollno,x):
+        self.name=name
+        self.rollno=rollno
+        self.cls=x
+stud1=student("Minar",2,"8th")
