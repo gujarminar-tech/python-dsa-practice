6a9dfdba32d40c4908537cd894ca630a5181a2d4
gujarminar-tech <gujarminar@gmail.com>
2026-04-16 00:09:27 +0530

    polymorphism

--- object oriented programming/02_oop.py
+++ object oriented programming/02_oop.py
@@ -6,3 +6,20 @@ protected | _name   |access in class and subclass
 private   | __name  |access only inside the class
 '''
 
+
+#Polymorphism
+#function overriding is an example of polymorphism
+class Animal:
+    def sound(self):
+        print("Hello")
+class Dog(Animal):
+    def sound(self):
+        print("Bark")
+class Cat(Animal):
+    def sound(self):
+        print("meow")
+
+obj1=Dog()
+obj1.sound() #Bark
+obj2=Cat()
+obj2.sound() #meow
