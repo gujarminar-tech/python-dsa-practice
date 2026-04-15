daa440fad65163d00a5b3e774a64b3b0259575df
gujarminar-tech <gujarminar@gmail.com>
2026-04-15 23:54:28 +0530

    Access modifier

---
+++ object oriented programming/02_oop.py
@@ -0,0 +1,8 @@
+#Access modifier 
+'''
+Type      | name    |access
+Public    | name    |accessed anywhare
+protected | _name   |access in class and subclass
+private   | __name  |access only inside the class
+'''
+
